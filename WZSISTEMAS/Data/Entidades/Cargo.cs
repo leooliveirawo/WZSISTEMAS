c38e3adc4eb4d645fb2236a24b3044dc18544ae4
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2023-08-18 12:37:57 -0300

    Removido os using sem uso do arquivo Cargo.

--- WZSISTEMAS/Data/Entidades/Cargo.cs
+++ WZSISTEMAS/Data/Entidades/Cargo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WZSISTEMAS.Data.Entidades
+﻿namespace WZSISTEMAS.Data.Entidades
 {
     public class Cargo
     {
