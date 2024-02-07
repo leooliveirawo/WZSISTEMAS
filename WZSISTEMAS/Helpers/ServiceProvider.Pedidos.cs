e29beede0fa63afd2374ff348c949f37320947db
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2024-02-07 08:58:57 -0300

    Implementado no arquivo ServiceProvider.Pedidos.cs o método que cria uma instância da classe FrmPedido e FrmPedidoManutencao.

---
+++ WZSISTEMAS/Helpers/ServiceProvider.Pedidos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WZSISTEMAS.Pedidos;
+
+namespace WZSISTEMAS.Helpers;
+public static partial class ServiceProvider
+{
+    public static FrmPedido FrmPedido(this IServiceProvider services)
+        => services.GetRequiredService<FrmPedido>();
+
+    public static FrmPedidoManutencao FrmPedidoManutencao(this IServiceProvider services)
+        => services.GetRequiredService<FrmPedidoManutencao>();
+}
