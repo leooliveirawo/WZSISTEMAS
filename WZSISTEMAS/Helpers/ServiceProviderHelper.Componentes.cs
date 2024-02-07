f0dcf2fb8ef7ed99fff4772e4f88fed6b8d7f4be
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2024-02-07 08:57:38 -0300

    Criado o método dentro do arquivo ServiceProviderHelper.Componentes.css que cria uma instância da classe FrmItemSelecionarPrecos.

---
+++ WZSISTEMAS/Helpers/ServiceProviderHelper.Componentes.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WZSISTEMAS.Componentes;
+
+namespace WZSISTEMAS.Helpers;
+public partial class ServiceProviderHelper
+{
+    public static FrmItemSelecionarPrecos FrmItemSelecionarPrecos(this IServiceProvider services)
+        => services.GetRequiredService<FrmItemSelecionarPrecos>();
+}
