f80912f3979da35da6ecbe118c7a10a45792242c
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2024-02-07 08:56:23 -0300

    Adicionado a funcionalidade de componentes (telas adicionais) e criado o componente para selecionar preços de um ítem.

---
+++ WZSISTEMAS/Componentes/FrmItemSelecionarPrecos.cs
@@ -0,0 +1,98 @@
+﻿using WZSISTEMAS.Dados.Servicos.Interfaces;
+using WZSISTEMAS.WinForms.Helpers;
+
+namespace WZSISTEMAS.Componentes;
+
+public partial class FrmItemSelecionarPrecos : Form
+{
+    private readonly IServicoProdutos servicoProdutos;
+
+    private long itemId;
+
+    private decimal preco1;
+    private decimal preco2;
+    private decimal preco3;
+
+    public decimal PrecoSelecionado { get; set; }
+
+    public FrmItemSelecionarPrecos(
+        IServicoProdutos servicoProdutos)
+    {
+        InitializeComponent();
+
+        this.servicoProdutos = servicoProdutos
+            ?? throw new ArgumentNullException(nameof(servicoProdutos));
+    }
+
+    public void DefinirProdutoId(long itemId)
+        => this.itemId = itemId;
+
+    private void FrmItemSelecionarPrecos_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            var item = servicoProdutos.ObterPorId(itemId);
+
+            if (item is null)
+            {
+                this.ExibirMensagem("O produto não foi encontrado.", "Produto não encontrado");
+
+                Close();
+            }
+            else
+            {
+                preco1 = item.PrecoFinal;
+                rbtnPreco1.Text = $"(F1) - {preco1:C2}";
+
+                if (item.PrecoFinal2.HasValue)
+                {
+                    preco2 = item.PrecoFinal2.Value;
+                    rbtnPreco2.Text = $"(F2) - {preco2:C2}";
+                }
+                else
+                    rbtnPreco2.Desativar();
+                if (item.PrecoFinal3.HasValue)
+                {
+                    preco3 = item.PrecoFinal3.Value;
+                    rbtnPreco3.Text = $"(F3) - {preco3:C2}";
+                }
+                else
+                    rbtnPreco3.Desativar();
+            }
+        }
+        catch (Exception erro)
+        {
+            this.ExibirMensagemErro(erro);
+        }
+    }
+
+    private void FrmItemSelecionarPrecos_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (Keys.Return.Comparar(e.KeyChar))
+        {
+            e.Handled = true;
+
+            PrecoSelecionado = rbtnPreco1.Checked ? preco1 : rbtnPreco2.Checked ? preco2 : preco3;
+
+            DialogResult = DialogResult.OK;
+        }
+        else if (Keys.Escape.Comparar(e.KeyChar))
+            e.Handled = true;
+    }
+
+    private void FrmItemSelecionarPrecos_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyData == Keys.F1)
+            rbtnPreco1.Marcar();
+        else if (e.KeyData == Keys.F2)
+        {
+            if (rbtnPreco2.Enabled)
+                rbtnPreco2.Marcar();
+        }
+        else if (e.KeyData == Keys.F3)
+        {
+            if (rbtnPreco3.Enabled)
+                rbtnPreco3.Marcar();
+        }
+    }
+}
