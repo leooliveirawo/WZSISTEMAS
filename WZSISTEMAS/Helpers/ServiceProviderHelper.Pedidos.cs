3b99bec95a3221d181116ba0b43096c70d256585
Leonardo Wizenfard de Oliveira <leooliveiraofs@gmail.com>
2024-02-07 09:12:47 -0300

    Alterado o nome do arquivo ServiceProvider.Pedidos.cs para ServiceProviderHelper.Pedidos.cs e criado o método que cria uma instância da classe FrmPedidoEmAberto.

--- WZSISTEMAS/Helpers/ServiceProviderHelper.Pedidos.cs
+++ WZSISTEMAS/Helpers/ServiceProviderHelper.Pedidos.cs
@@ -2,11 +2,14 @@
 using WZSISTEMAS.Pedidos;
 
 namespace WZSISTEMAS.Helpers;
-public static partial class ServiceProvider
+public static partial class ServiceProviderHelper
 {
     public static FrmPedido FrmPedido(this IServiceProvider services)
         => services.GetRequiredService<FrmPedido>();
 
     public static FrmPedidoManutencao FrmPedidoManutencao(this IServiceProvider services)
         => services.GetRequiredService<FrmPedidoManutencao>();
+
+    public static FrmPedidoEmAberto FrmPedidoEmAberto(this IServiceProvider services)
+        => services.GetRequiredService<FrmPedidoEmAberto>();
 }
